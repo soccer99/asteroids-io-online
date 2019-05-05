3bfea6a1755dac336a6c620c9fc291d66a196c65
Brady Endres <brady.endres@mpulsemobile.com>
2019-05-04 20:58:44 -0700

    movement works

--- client/src/app.ts
+++ client/src/app.ts
@@ -87,9 +87,104 @@ var ship;
 var lastFired;
 var currentSpeed = 0;
 let active = true;
-let lastPositionUpdate;
+let lastPositionUpdate = 0;
+let ws;
+let player_id;
+let enemyShips = {};
+
+let startingX;
+let startingY;
+let startingRotation;
+let enemyAcceleration = {};
 
 function create(): void {
+  // Websocket init
+  // initialize websocket with event listeners
+  var mws = () => {
+    let ws = new WebSocket('ws://localhost:8888/gamesocket')
+    ws.onopen = (e) => {
+      console.log("opened");
+      console.log(e)
+    }
+    ws.onmessage = (e) => {
+    console.log("ws.onmessage")
+      let data = JSON.parse(e.data)
+      console.log(data.type)
+      if (data.type == "INITIAL_STATE") {
+        player_id = data.settings.player_id
+
+        ship.x = data.state.players[player_id].x
+        ship.y = data.state.players[player_id].y
+        ship.rotation = data.state.players[player_id].rotation
+
+        // TODO : loop through all players and create them
+        for (let key in data.state.players) {
+          if (player_id != key) {
+            let p = data.state.players[key]
+            enemyShips[key] = this.physics.add.image(p.x, p.y, 'ship');
+  
+            enemyShips[key].setDamping(true);
+            enemyShips[key].setDrag(0.99);
+            enemyShips[key].setMaxVelocity(200);
+  
+            enemyShips[key].rotation = p.rotation
+            // enemyShips[key].body.acceleration = p.acceleration
+          }
+        }
+      } else if (data.type == "PLAYER_JOINED") {
+        console.log("Player Joined!")
+        console.log(data)
+        let new_player = data.new_player
+        let new_player_id = new_player.id
+        enemyShips[new_player_id] = this.physics.add.image(new_player.x, new_player.y, 'ship');
+
+        enemyShips[new_player_id].setDamping(true);
+        enemyShips[new_player_id].setDrag(0.99);
+        enemyShips[new_player_id].setMaxVelocity(200);
+
+        enemyShips[new_player_id].rotation = new_player.rotation
+      } else if (data.type == "PLAYER_LEFT") {
+        console.log("Player left!")
+        console.log(data)
+      } else if (data.type == "PLAYER_POSITION") {
+        // for (let i=0; i<data.)
+      } else if (data.type == "PLAYER_KEY") {
+        console.log("player key event")
+        console.log(data.state.players)
+        for (let other_id in data.state.players) {
+          if (other_id != player_id) {
+            let p = data.state.players[other_id]
+            enemyShips[other_id].x = p.x
+            enemyShips[other_id].y = p.y
+            enemyShips[other_id].rotation = p.rotation
+            // enemyShips[other_id].body.acceleration = p.acceleration
+
+            if (p.keys["left"]) {
+              enemyShips[other_id].setAngularVelocity(-300);
+            } else if (p.keys["right"]) {
+                enemyShips[other_id].setAngularVelocity(300);
+            } else {
+              enemyShips[other_id].setAngularVelocity(0);
+            }
+
+            enemyAcceleration[other_id] = p.keys["up"]
+            // if (p.keys["up"] == true) {
+            // } else if (p.keys["up"] == false) {
+            //   enemyAcceleration[other_id] = p.keys["up"]
+            // }
+          }
+        }
+      }
+    }
+    ws.onclose = (e) => {
+      console.log("closed");
+      console.log(e)}
+      return ws
+    }
+
+  ws = mws()
+
+
   // let playerBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
   // bullets = this.add.group({
   //   classType: Bullet,
@@ -108,8 +203,22 @@ function create(): void {
   ship.setDrag(0.99);
   ship.setMaxVelocity(200);
 
+  
+
   // Fires bullet from player on left click of mouse
-//   this.input.keyboard.on('keydown_SPACE', function (pointer, time, lastFired) {
+  this.input.keyboard.on('keydown_SPACE', function (pointer, time, lastFired) {
+    var jsn = JSON.stringify({
+      "message_type": "PLAYER_POSITION",
+      "data": {
+        "id": player_id,
+        "x": ship.x,
+        "y": ship.y,
+        "rotation": ship.rotation,
+        "velocity": 5
+      }
+    })
+    // send
+    ws.send(jsn)
 //     if (active === false)
 //       return;
 
@@ -122,7 +231,7 @@ function create(): void {
 //         this.physics.velocityFromRotation(bullet.rotation, 200, bullet.body.acceleration);
 //         // this.physics.add.collider(enemy, bullet, enemyHitCallback);
 //     }
-// }, this);
+}, this);
 
   // this.sound.play('shoot');
 
@@ -138,35 +247,68 @@ function create(): void {
   // this.physics.enable(ship, Phaser.Physics.Arcade);
 
   cursors = this.input.keyboard.createCursorKeys();
+
+  this.input.keyboard.on('keydown_LEFT', function (event) {
+    send_key("left", true)
+  })
+  this.input.keyboard.on('keyup_LEFT', function (event) {
+    send_key("left", false)
+  })
+  this.input.keyboard.on('keydown_RIGHT', function (event) {
+    send_key("right", true)
+  })
+  this.input.keyboard.on('keyup_RIGHT', function (event) {
+    send_key("right", false)
+  })
+  this.input.keyboard.on('keydown_UP', function (event) {
+    send_key("up", true)
+  })
+  this.input.keyboard.on('keyup_UP', function (event) {
+    send_key("up", false)
+  })
+}
+
+function send_key(key, pressed) {
+  var jsn = JSON.stringify({
+    "message_type": "PLAYER_KEY",
+    "data": {
+      "id": player_id,
+      "x": ship.x,
+      "y": ship.y,
+      "rotation": ship.rotation,
+      "acceleration": ship.body.acceleration,
+      "key": key,
+      "pressed": pressed
+    }
+  })
+  // send
+  ws.send(jsn)
 }
 
 function update(time, delta): void {
-
-  if (time > lastPositionUpdate) {
-    // Send position update
-  }
-  lastPositionUpdate += 5;
-  
-  if (cursors.left.isDown)
-  {
+  if (cursors.left.isDown) {
       ship.setAngularVelocity(-300);
   }
-  else if (cursors.right.isDown)
-  {
+  else if (cursors.right.isDown) {
       ship.setAngularVelocity(300);
-  }
-  else
-  {
-      ship.setAngularVelocity(0);
+  } else {
+    ship.setAngularVelocity(0);
   }
 
-    if (cursors.up.isDown)
-    {
+    if (cursors.up.isDown) {
         this.physics.velocityFromRotation(ship.rotation, 200, ship.body.acceleration);
-    }
-    else
-    {
+    } else {
         ship.setAcceleration(0);
+    }
+
+    for (let other_ship_id in enemyAcceleration) {
+      if (other_ship_id != player_id) {
+        if (enemyAcceleration[other_ship_id] == true) {
+          this.physics.velocityFromRotation(enemyShips[other_ship_id].rotation, 200, enemyShips[other_ship_id].body.acceleration);
+        } else {
+          enemyShips[other_ship_id].setAcceleration(0);
+        }
+      }
     }
 
     // if (cursors.spaceKey.isDown && time > lastFired)
@@ -182,4 +324,7 @@ function update(time, delta): void {
     // }
 
     this.physics.world.wrap(ship, 32);
+    for (let key in enemyShips) {
+      this.physics.world.wrap(enemyShips[key], 32);
+    }
 }
--- server/app.py
+++ server/app.py
@@ -42,19 +42,37 @@ class GameSocketHandler(tornado.websocket.WebSocketHandler):
     waiters = set()
     cache = {"players": {}, "bullets": []}
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) # :)
+        self.player_id = None
+
     def get_compression_options(self):
         # Non-None enables compression with default options.
         return {}
 
+    def check_origin(self, origin):
+        return True
+
     def open(self):
+        print("Player connected")
         GameSocketHandler.waiters.add(self)
         players = self.cache.get("players")
         new_player_id = str(uuid.uuid4())
+
+        # Store player id on this handler instance
+        self.player_id = new_player_id
+
         new_player = {
+            "id": new_player_id,
             "x": random.randint(0, 800),
             "y": random.randint(0, 600),
-            "direction": random.randint(0, 360),
-            "velocity": 0,
+            "rotation": random.randint(0, 360),
+            "acceleration": 0,
+            "keys": {
+                "up": False,
+                "left": False,
+                "right": False
+            }
         }
         players[new_player_id] = new_player
         # Send back the game settings
@@ -62,6 +80,7 @@ class GameSocketHandler(tornado.websocket.WebSocketHandler):
             {
                 "status": "success",
                 "state": self.cache,
+                "type": "INITIAL_STATE",
                 "settings": {
                     "player_id": new_player_id,
                     "room_friction": 1,
@@ -74,9 +93,33 @@ class GameSocketHandler(tornado.websocket.WebSocketHandler):
                 },
             }
         )
+        for waiter in self.waiters:
+            if waiter == self:
+                continue
+            try:
+                waiter.write_message(
+                    {
+                        "type": PLAYER_JOINED,
+                        "new_player": new_player,
+                    }
+                )
+            except:
+                logging.error("Error sending message", exc_info=True)
 
     def on_close(self):
+        print("Player disconnected")
         GameSocketHandler.waiters.remove(self)
+
+        for waiter in self.waiters:
+            try:
+                waiter.write_message(
+                    {
+                        "type": PLAYER_LEFT,
+                        "player_id": self.player_id,
+                    }
+                )
+            except:
+                logging.error("Error sending message", exc_info=True)
 
     @classmethod
     def update_cache(cls, message):
@@ -89,29 +132,38 @@ class GameSocketHandler(tornado.websocket.WebSocketHandler):
                 if pid == data.get("id"):
                     p["x"] = data.get("x")
                     p["y"] = data.get("y")
-                    p["direction"] = data.get("direction")
-                    p["velocity"] = data.get("velocity")
+                    p["rotation"] = data.get("rotation")
+                    p["acceleration"] = data.get("acceleration")
         elif message_type == PLAYER_SHOOT:
             cls.cache["bullets"].append(
                 {
                     "player_id": data.get("player_id"),
                     "startx": data.get("startx"),
                     "starty": data.get("starty"),
-                    "direction": data.get("direction"),
+                    "rotation": data.get("rotation"),
                     "speed": BULLET_SPEED,
                 }
             )
+        elif message_type == PLAYER_KEY:
+            for pid, p in cls.cache.get("players").items():
+                if pid == data.get("id"):
+                    p["x"] = data.get("x")
+                    p["y"] = data.get("y")
+                    p["rotation"] = data.get("rotation")
+                    p["acceleration"] = data.get("acceleration")
+                    p["keys"][data.get("key")] = data.get("pressed")
 
     @classmethod
     def send_updates(cls, data):
         logging.info("sending message to %d waiters", len(cls.waiters))
         for waiter in cls.waiters:
             try:
-                waiter.write_message({"state": cls.cache})
+                waiter.write_message({"state": cls.cache, "type": data.get("message_type")})
             except:
                 logging.error("Error sending message", exc_info=True)
 
     def on_message(self, message):
+        print("on message")
         logging.info("got message %r", message)
         parsed = tornado.escape.json_decode(message)
 
